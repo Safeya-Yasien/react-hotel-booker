5da53c13d4780fd87ca147c5385781945a9ac416
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-26 06:11:27 +0200

    add date range calender

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,8 +7,23 @@ import {
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { format } from "date-fns";
+import { useState } from "react";
+
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
 
 const Header = () => {
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
   return (
     <div className="header bg-[#003580] text-white flex justify-center relative">
       <div className="headerContainer container mx-auto w-full max-w-5xl mt-5 mb-[100px]">
@@ -45,6 +60,60 @@ const Header = () => {
           <button className="headerBtn bg-[#0071c2] text-white font-medium border-none p-[10px] cursor-pointer">
             Sign in / Register
           </button>
+
+          <div className="headerSearch h-[60px] bg-white border-[3px] border-[#febb02] flex items-center justify-around py-[10px] rounded-[5px] absolute -bottom-[30px] w-full max-w-5xl ">
+            <div className="headerSearchItem flex items-center gap-[10px]">
+              <FontAwesomeIcon
+                icon={faBed}
+                className="headerIcon text-gray-400"
+              />
+              <input
+                type="text"
+                placeholder="Where are you going?"
+                className="headerSearchInput border-none outline-none"
+              />
+            </div>
+            <div className="headerSearchItem flex items-center gap-[10px]">
+              <FontAwesomeIcon
+                icon={faCalendarDays}
+                className="headerIcon text-gray-400"
+              />
+              <span
+                onClick={() => setOpenDate(!openDate)}
+                className="headerSearchText text-gray-400 cursor-pointer"
+              >
+                {`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                  date[0].endDate,
+                  "MM/dd/yyy"
+                )}`}
+              </span>
+              {openDate && (
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={(item) => setDate([item.selection])}
+                  moveRangeOnFirstSelection={false}
+                  ranges={date}
+                  className="date absolute top-[50px] z-10"
+                />
+              )}
+            </div>
+
+            <div className="headerSearchItem flex items-center gap-[10px]">
+              <FontAwesomeIcon
+                icon={faPerson}
+                className="headerIcon text-gray-400"
+              />
+              <span className="headerSearchText text-gray-400 cursor-pointer">
+                2 adults 2 children 1 room to date
+              </span>
+            </div>
+
+            <div className="headerSearchItem flex items-center gap-[10px]">
+              <button className="headerBtn bg-[#0071c2] text-white font-medium border-none p-[10px] cursor-pointer">
+                Search
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
