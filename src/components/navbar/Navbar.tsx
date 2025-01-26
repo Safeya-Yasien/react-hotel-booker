d02a44b44978950660bb2fca5f41f68f39493b2c
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-26 03:42:24 +0200

    update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 const Navbar = () => {
   return (
-    <div className="navbar h-[50px] bg-[#003580] flex justify-center ">
-      <div className="container w-full max-w-[1024px] text-white flex items-center justify-between">
+    <div className="navbar h-[50px] bg-[#003580] flex justify-center text-white ">
+      <div className="container w-full max-w-[1024px] flex items-center justify-between">
         <span className="logo font-medium">lamabooking</span>
-        <div className="navItems">
-          <button className="navButton border-none px-[10px] py-[5px] text-[#003580] cursor-pointer">
+        <div className="navItems flex items-center gap-5">
+          <button className="navButton border-none px-[10px] py-[5px] text-[#003580] cursor-pointer bg-white">
             Register
           </button>
-          <button className="navButton border-none px-[10px] py-[5px] text-[#003580] cursor-pointer">
+          <button className="navButton border-none px-[10px] py-[5px] text-[#003580] cursor-pointer bg-white">
             Login
           </button>
         </div>
