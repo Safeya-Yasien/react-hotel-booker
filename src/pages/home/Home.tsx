2ad473bb8ccb48f27014043c6a0e5dd5403e98c5
Safeya-Yasien <ssafeya5@gmail.com>
2025-01-27 05:07:24 +0200

    update App.tsx, Header.tsx, Navbar.tsx and Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 
@@ -5,16 +6,17 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-     <Header />
-      {/*  <div className="homeContainer">
-        <Featured />
-        <h1 className="homeTitle">Browse by property type</h1>
+      <Header type="" />
+      <div className="homeContainer mt-[50px] flex flex-col items-center gap-[30px] ">
+        <Featured />{" "}
+        {/*  
+        <h1 className="homeTitle max-w-5xl text-[20px]">Browse by property type</h1>
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
         <MailList />
-        <Footer />
-      </div> */}
+        <Footer />*/}
+      </div>
     </div>
   );
 };
